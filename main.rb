30459f5e3287b371397c025e631a60144dae0d86
Dat Le-Tien <vnkeymaster@gmail.com>
2020-02-11 14:59:38 +0700

    Refactor main, use zeitwerk

--- main.rb
+++ main.rb
@@ -1,113 +1,135 @@
 #!/usr/bin/ruby
-
+require 'rubyXL'
+require 'roo'
 require 'optparse'
 require 'optparse/time'
 require 'ostruct'
 require 'pp'
 
-class OptparseRcs
+# Begin autoloading
+require 'zeitwerk'
+require 'listen'
+loader = Zeitwerk::Loader.new
+loader.enable_reloading
 
-  CODES = %w[iso-2022-jp shift_jis euc-jp utf8 binary]
-  CODE_ALIASES = { "jis" => "iso-2022-jp", "sjis" => "shift_jis" }
+loader.push_dir("lib")
+loader.push_dir("script")
 
-  #
-  # Return a structure describing the options.
-  #
-  def self.parse(args)
-    # The options specified on the command line will be collected in *options*.
-    # We set default values here.
-    options = OpenStruct.new
-    options.library = []
-    options.inplace = false
-    options.encoding = "utf8"
-    options.transfer_type = :auto
-    options.verbose = false
+loader.setup
 
-    opt_parser = OptionParser.new do |opts|
-      opts.banner = "Usage: main.rb [options]"
+Listen.to("lib", "script") { loader.reload }.start
+loader.eager_load
+# End autoloading
 
-      opts.separator ""
-      opts.separator "Specific options:"
+# class OptparseRcs
 
-      # Mandatory argument.
-      opts.on("-r", "--require LIBRARY",
-              "Require the LIBRARY before executing your script") do |lib|
-        options.library << lib
-      end
+#   CODES = %w[iso-2022-jp shift_jis euc-jp utf8 binary]
+#   CODE_ALIASES = { "jis" => "iso-2022-jp", "sjis" => "shift_jis" }
 
-      # Optional argument; multi-line description.
-      opts.on("-i", "--inplace [EXTENSION]",
-              "Edit ARGV files in place",
-              "  (make backup if EXTENSION supplied)") do |ext|
-        options.inplace = true
-        options.extension = ext || ''
-        options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
-      end
+#   #
+#   # Return a structure describing the options.
+#   #
+#   def self.parse(args)
+#     # The options specified on the command line will be collected in *options*.
+#     # We set default values here.
+#     options = OpenStruct.new
+#     options.library = []
+#     options.inplace = false
+#     options.encoding = "utf8"
+#     options.transfer_type = :auto
+#     options.verbose = false
 
-      # Cast 'delay' argument to a Float.
-      opts.on("--delay N", Float, "Delay N seconds before executing") do |n|
-        options.delay = n
-      end
+#     opt_parser = OptionParser.new do |opts|
+#       opts.banner = "Usage: main.rb [options]"
 
-      # Cast 'time' argument to a Time object.
-      opts.on("-t", "--time [TIME]", Time, "Begin execution at given time") do |time|
-        options.time = time
-      end
+#       opts.separator ""
+#       opts.separator "Specific options:"
 
-      # Cast to octal integer.
-      opts.on("-F", "--irs [OCTAL]", OptionParser::OctalInteger,
-              "Specify record separator (default \\0)") do |rs|
-        options.record_separator = rs
-      end
+#       # Mandatory argument.
+#       opts.on("-r", "--require LIBRARY",
+#               "Require the LIBRARY before executing your script") do |lib|
+#         options.library << lib
+#       end
 
-      # List of arguments.
-      opts.on("--list x,y,z", Array, "Example 'list' of arguments") do |list|
-        options.list = list
-      end
+#       # Optional argument; multi-line description.
+#       opts.on("-i", "--inplace [EXTENSION]",
+#               "Edit ARGV files in place",
+#               "  (make backup if EXTENSION supplied)") do |ext|
+#         options.inplace = true
+#         options.extension = ext || ''
+#         options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+#       end
 
-      # Keyword completion.  We are specifying a specific set of arguments (CODES
-      # and CODE_ALIASES - notice the latter is a Hash), and the user may provide
-      # the shortest unambiguous text.
-      code_list = (CODE_ALIASES.keys + CODES).join(',')
-      opts.on("--code CODE", CODES, CODE_ALIASES, "Select encoding",
-              "  (#{code_list})") do |encoding|
-        options.encoding = encoding
-      end
+#       # Cast 'delay' argument to a Float.
+#       opts.on("--delay N", Float, "Delay N seconds before executing") do |n|
+#         options.delay = n
+#       end
 
-      # Optional argument with keyword completion.
-      opts.on("--type [TYPE]", [:text, :binary, :auto],
-              "Select transfer type (text, binary, auto)") do |t|
-        options.transfer_type = t
-      end
+#       # Cast 'time' argument to a Time object.
+#       opts.on("-t", "--time [TIME]", Time, "Begin execution at given time") do |time|
+#         options.time = time
+#       end
 
-      # Boolean switch.
-      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-        options.verbose = v
-      end
+#       # Cast to octal integer.
+#       opts.on("-F", "--irs [OCTAL]", OptionParser::OctalInteger,
+#               "Specify record separator (default \\0)") do |rs|
+#         options.record_separator = rs
+#       end
 
-      opts.separator ""
-      opts.separator "Common options:"
+#       # List of arguments.
+#       opts.on("--list x,y,z", Array, "Example 'list' of arguments") do |list|
+#         options.list = list
+#       end
 
-      # No argument, shows at tail.  This will print an options summary.
-      # Try it and see!
-      opts.on_tail("-h", "--help", "Show this message") do
-        puts opts
-        exit
-      end
+#       # Keyword completion.  We are specifying a specific set of arguments (CODES
+#       # and CODE_ALIASES - notice the latter is a Hash), and the user may provide
+#       # the shortest unambiguous text.
+#       code_list = (CODE_ALIASES.keys + CODES).join(',')
+#       opts.on("--code CODE", CODES, CODE_ALIASES, "Select encoding",
+#               "  (#{code_list})") do |encoding|
+#         options.encoding = encoding
+#       end
 
-      # Another typical switch to print the version.
-      opts.on_tail("--version", "Show version") do
-        puts ["0","0","1"].join('.')
-        exit
-      end
-    end
+#       # Optional argument with keyword completion.
+#       opts.on("--type [TYPE]", [:text, :binary, :auto],
+#               "Select transfer type (text, binary, auto)") do |t|
+#         options.transfer_type = t
+#       end
 
-    opt_parser.parse!(args)
-    options
-  end  # parse()
+#       # Boolean switch.
+#       opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+#         options.verbose = v
+#       end
 
-end  # class OptparseRcs
+#       opts.separator ""
+#       opts.separator "Common options:"
 
-options = OptparseRcs.parse(ARGV)
-pp options
-pp ARGV
+#       # No argument, shows at tail.  This will print an options summary.
+#       # Try it and see!
+#       opts.on_tail("-h", "--help", "Show this message") do
+#         puts opts
+#         exit
+#       end
+
+#       # Another typical switch to print the version.
+#       opts.on_tail("--version", "Show version") do
+#         puts ["0","0","1"].join('.')
+#         exit
+#       end
+#     end
+
+#     opt_parser.parse!(args)
+#     options
+#   end  # parse()
+
+# end  # class OptparseRcs
+
+# options = OptparseRcs.parse(ARGV)
+# pp options
+# pp ARGV
+
+input = 'input/Reapra_consolidation_FY2017_(dummy).xlsx'
+balance_sheet = 'BalanceSheet 15-8-19'
+# income_statement = 'IncomeStatement 15-8-19'
+NetsuiteConsolidationReport.new(input, balance_sheet).execute
+
