da6ea1211139714e0c83d9d77797cb79c81a6a0f
Dat Le-Tien <vnkeymaster@gmail.com>
2020-02-14 13:50:11 +0700

    Implement clone_entities_with_ref_formulas

--- script/netsuite_consolidation_report.rb
+++ script/netsuite_consolidation_report.rb
@@ -48,6 +48,38 @@ class NetsuiteConsolidationReport
     sheet_writer.write_output
   end
 
+  def clone_entities_with_ref_formulas
+    left = get_coor_roo sheet_props.left.join
+    right = get_coor_roo sheet_props.right.join
+
+    # clone skeleton
+    x = left.first
+    is_formula = true
+    while x < right.first  do
+      y = left.last
+      while y < right.last do
+        if x == left.first || y == left.last
+          data = "\'#{sheet_name}\'!#{get_ref(coor_roo2_coor_rubyxl([x,y]))}"
+          sheet_writer.write data, coor_roo2_coor_rubyxl([x,y]), is_formula
+        else
+          apply_vlookup_formulas x, y
+        end
+        y += 1
+      end
+      x +=1
+      puts "Row: #{x}"
+    end
+  end
+
+  def apply_vlookup_formulas x,y
+    get_segments.each {|r|
+      if r.include? x
+        data = "VLOOKUP(A#{x},\'#{sheet_name}\'!#{sheet_props.locked_full_coor},#{y},FALSE)"
+        sheet_writer.write data, coor_roo2_coor_rubyxl([x,y]), true
+      end
+    }
+  end
+
   private
     def sheet_reader
       @sheet_reader ||= Sheets::SheetReader.new input, sheet_name
