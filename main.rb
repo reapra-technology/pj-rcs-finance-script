291e6431827ca1cd2c4918157c244ecd4fbb38db
Dat Le-Tien <vnkeymaster@gmail.com>
2020-02-19 11:32:46 +0700

    Add script invoke CLI (ongoing)

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/ruby
-require 'rubyXL'
-require 'roo'
+
 require 'optparse'
 require 'optparse/time'
 require 'ostruct'
 require 'pp'
+require 'rubyXL'
+require 'roo'
+require 'pry'
+require 'pry-byebug'
 
 # Begin autoloading
 require 'zeitwerk'
@@ -21,115 +24,137 @@ Listen.to("lib", "script") { loader.reload }.start
 loader.eager_load
 # End autoloading
 
-# class OptparseRcs
+class OptParser
 
-#   CODES = %w[iso-2022-jp shift_jis euc-jp utf8 binary]
-#   CODE_ALIASES = { "jis" => "iso-2022-jp", "sjis" => "shift_jis" }
+  CODES = %w[iso-2022-jp shift_jis euc-jp utf8 binary]
+  CODE_ALIASES = { "jis" => "iso-2022-jp", "sjis" => "shift_jis" }
 
-#   #
-#   # Return a structure describing the options.
-#   #
-#   def self.parse(args)
-#     # The options specified on the command line will be collected in *options*.
-#     # We set default values here.
-#     options = OpenStruct.new
-#     options.library = []
-#     options.inplace = false
-#     options.encoding = "utf8"
-#     options.transfer_type = :auto
-#     options.verbose = false
+  #
+  # Return a structure describing the options.
+  #
+  def self.parse(args)
+    # The options specified on the command line will be collected in *options*.
+    # We set default values here.
+    options = OpenStruct.new
+    options.input = nil
+    options.output = nil
+    options.bs = nil
+    options.is = nil
+    options.cje = nil
+    options.process = []
+    options.verbose = false
 
-#     opt_parser = OptionParser.new do |opts|
-#       opts.banner = "Usage: main.rb [options]"
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: main.rb [options]"
 
-#       opts.separator ""
-#       opts.separator "Specific options:"
+      opts.separator ""
+      opts.separator "Specific options:"
 
-#       # Mandatory argument.
-#       opts.on("-r", "--require LIBRARY",
-#               "Require the LIBRARY before executing your script") do |lib|
-#         options.library << lib
-#       end
+      # Mandatory argument.
+      opts.on("-i", "--input EXCELX",
+              "The xlsx file which is export from Netsuite") do |input|
+        options.input = input
+      end
 
-#       # Optional argument; multi-line description.
-#       opts.on("-i", "--inplace [EXTENSION]",
-#               "Edit ARGV files in place",
-#               "  (make backup if EXTENSION supplied)") do |ext|
-#         options.inplace = true
-#         options.extension = ext || ''
-#         options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
-#       end
+      opts.on("-o", "--output EXCELX",
+              "The xlsx file to save after processed") do |output|
+        options.output = output || "./output/output.xlsx"
+      end
 
-#       # Cast 'delay' argument to a Float.
-#       opts.on("--delay N", Float, "Delay N seconds before executing") do |n|
-#         options.delay = n
-#       end
+      # Optional argument; multi-line description.
+      opts.on("--balancesheet=STRING",
+              "The balance sheet you need to proceed to generate consolidation report",
+              "  (optional)") do |bs|
+        options.bs = bs
+        # options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+      end
 
-#       # Cast 'time' argument to a Time object.
-#       opts.on("-t", "--time [TIME]", Time, "Begin execution at given time") do |time|
-#         options.time = time
-#       end
+      opts.on("--incomestatement=STRING",
+              "The income statement you need to proceed to generate consolidation report",
+              "  (optional)") do |is|
+        options.is = is
+        # options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+      end
 
-#       # Cast to octal integer.
-#       opts.on("-F", "--irs [OCTAL]", OptionParser::OctalInteger,
-#               "Specify record separator (default \\0)") do |rs|
-#         options.record_separator = rs
-#       end
+      opts.on("--cje=STRING",
+              "The CJE sheet for referencing in Consolidation BS/IC",
+              "  default: \"CJE\" (optional)") do |cje|
+        options.cje = cje || "CJE"
+        # options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+      end
 
-#       # List of arguments.
-#       opts.on("--list x,y,z", Array, "Example 'list' of arguments") do |list|
-#         options.list = list
-#       end
+      # List of arguments.
+      opts.on("-p", "--process cje,bs,is", Array, "To let script execute determined tasks",
+        "  cje: generate CJE sheet (which is mandatory for BS/IS process)",
+        "   bs: generate Consolidation_BS sheet",
+        "   is: generate Consolidation_IS sheet") do |process|
+        options.process = process
+      end
 
-#       # Keyword completion.  We are specifying a specific set of arguments (CODES
-#       # and CODE_ALIASES - notice the latter is a Hash), and the user may provide
-#       # the shortest unambiguous text.
-#       code_list = (CODE_ALIASES.keys + CODES).join(',')
-#       opts.on("--code CODE", CODES, CODE_ALIASES, "Select encoding",
-#               "  (#{code_list})") do |encoding|
-#         options.encoding = encoding
-#       end
+      # Boolean switch.
+      opts.on("-v", "--[no-]verbose", "Run verbosely with log") do |v|
+        options.verbose = v
+      end
 
-#       # Optional argument with keyword completion.
-#       opts.on("--type [TYPE]", [:text, :binary, :auto],
-#               "Select transfer type (text, binary, auto)") do |t|
-#         options.transfer_type = t
-#       end
+      opts.separator ""
+      opts.separator "Common options:"
 
-#       # Boolean switch.
-#       opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-#         options.verbose = v
-#       end
+      # No argument, shows at tail.  This will print an options summary.
+      # Try it and see!
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
 
-#       opts.separator ""
-#       opts.separator "Common options:"
+      # Another typical switch to print the version.
+      opts.on_tail("--version", "Show semver") do
+        puts ["0","9","1"].join('.')
+        exit
+      end
 
-#       # No argument, shows at tail.  This will print an options summary.
-#       # Try it and see!
-#       opts.on_tail("-h", "--help", "Show this message") do
-#         puts opts
-#         exit
-#       end
+      opts.on_tail("--example", "Show example command line"
+        ) do
+        puts "---- TO GENERATE CJE ----"
+        puts "bundle exec ruby main.rb -i 'input/consolidation.xlsx' -o 'output/new_consolidation.xlsx' -p cje --cje=CJE"
+        puts
+        puts "---- TO GENERATE CONSOLIDATION REPORTS ----"
+        puts "bundle exec ruby main.rb -i 'input/consolidation.xlsx' -o 'output/new_consolidation.xlsx' -p bs,is --balancesheet='BalanceSheet 15-8-19' --incomestatement='IncomeStatement 15-8-19' --cje=CJE"
+        puts
+        puts "---- TO GENERATE CONSOLIDATION BALANCE SHEET WITH ERRORS / PROCESSING STATUS ----"
+        puts "bundle exec ruby main.rb -i 'input/consolidation.xlsx' -o 'output/new_consolidation.xlsx' -p bs --balancesheet='BalanceSheet 15-8-19' --cje=CJE -v"
 
-#       # Another typical switch to print the version.
-#       opts.on_tail("--version", "Show version") do
-#         puts ["0","0","1"].join('.')
-#         exit
-#       end
-#     end
+        exit
+      end
+    end
 
-#     opt_parser.parse!(args)
-#     options
-#   end  # parse()
+    opt_parser.parse!(args)
+    options
+  end  # parse()
 
-# end  # class OptparseRcs
+end  # class OptParser
 
-# options = OptparseRcs.parse(ARGV)
-# pp options
-# pp ARGV
+options = OptParser.parse(ARGV)
+pp options
 
-input = 'input/Reapra_consolidation_FY2017_(dummy).xlsx'
-balance_sheet = 'BalanceSheet 15-8-19'
-# income_statement = 'IncomeStatement 15-8-19'
-NetsuiteConsolidationReport.new(input, balance_sheet).prerequisite
+# check required arguments
+raise OptionParser::MissingArgument.new("input") if options[:input].nil?
+raise OptionParser::MissingArgument.new("bs") if options[:bs].nil? && options[:p].include?("bs")
+raise OptionParser::MissingArgument.new("is") if options[:is].nil? && options[:p].include?("is")
+raise OptionParser::MissingArgument.new("process cje require both balancesheet and incomestatement to be listed") if (options[:is].nil? || options[:bs].nil?) && options[:p].include?("cje")
+raise OptionParser::MissingArgument.new("no process defined to run") if options[:p].empty?
+
+# FIXME refactor if below to be switch case
+if options[:p].include? "cje"
+  NetsuiteConsolidationReportCjeBuilder.new(options[:input], options[:bs], options[:is], options[:cje], options[:v]).build_cje_sheet
+
+  return
+end
+
+if options[:p].include?("bs") || options[:p].include?("is")
+  ns = NetsuiteConsolidationReport.new(options[:input], income_statement, options[:cje], options[:v]).prerequisite
+  ns.clone_entities_with_ref_formulas
+  ns.run
+
+  return
+end
 
